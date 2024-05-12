c4b6ca21554ee1de8007e6f60ddcf4928cefc72e
Levangul <150108077+Levangul@users.noreply.github.com>
2024-05-11 23:40:52 -0400

    deploy

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,57 +1,30 @@
-import { FaHtml5, FaCss3, FaReact, FaNodeJs, FaDatabase } from "react-icons/fa";
-import { SiMongodb, SiExpress, SiMysql, SiJavascript } from "react-icons/si";
-import { FiClipboard } from "react-icons/fi"; 
+import ResumeImage from "../assets/6CF567B8-F852-428F-B840-D2F139162A69_1_105_c.jpeg";
 
-const Portfolio = () => {
+const Resume = () => {
   return (
-    
-    <div className="container skills">
-      
-      <div className="row justify-content-center ">
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <FaHtml5 size={40} color="#E34F26" />
-          <p className="mt-2">HTML5</p>
+    <div className="font-roboto mx-auto my-10 max-w-4xl p-8 rounded-lg shadow-lg text-center">
+      <h1 className="text-4xl font-bold text-indigo-600 mb-6">Resume</h1>
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center">
+          <img
+            src={ResumeImage}
+            alt="Resume"
+            className="w-full max-w-full border border-gray-300 rounded shadow"
+            style={{ maxWidth: "100%" }}
+          />
         </div>
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <FaCss3 size={40} color="#264DE4" />
-          <p className="mt-2">CSS3</p>
-        </div>
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <SiJavascript size={40} color="#F7DF1E" />
-          <p className="mt-2">JavaScript</p>
-        </div>
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <FaReact size={40} color="#61DAFB" />
-          <p className="mt-2">React</p>
-        </div>
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <FaNodeJs size={40} color="#339933" />
-          <p className="mt-2">Node.js</p>
-        </div>
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <FaDatabase size={40} color="#336791" />
-          <p className="mt-2">Database</p>
-        </div>
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <SiMongodb size={40} color="#47A248" /> 
-          <p className="mt-2">MongoDB</p>
-        </div>
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <SiExpress size={40} color="#000000" /> 
-          <p className="mt-2">Express</p>
-        </div>
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <SiMysql size={40} color="#00758F" /> 
-          <p className="mt-2">MySQL</p>
-        </div>
-        <div className="col-lg-2 col-md-3 col-sm-4 col-6 text-center mb-4">
-          <FiClipboard size={40} color="#99424F" />
-          <p className="mt-2">Jest</p>
+        <div className="mt-4">
+          <a
+            href={ResumeImage}
+            download
+            className= "text-white font-bold py-2 px-4 rounded-full "
+          >
+            Download
+          </a>
         </div>
       </div>
     </div>
-   
   );
 };
 
-export default Portfolio;
+export default Resume;
